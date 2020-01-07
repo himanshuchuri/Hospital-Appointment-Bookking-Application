d202949bcb911e69d905d7cc4c75b45381e57b5d
himanshuchuri <himanshuchuri@gmail.com>
2020-01-07 16:53:59 +0530

    Initial Load (only basic )

---
+++ android/app/src/main/kotlin/com/appointment/flutter_doctor_application/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appointment.flutter_doctor_application
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
